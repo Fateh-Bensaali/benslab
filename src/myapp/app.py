44a1385e708db64c3d03d27b99b06b321dc60382
Fateh Bensaali <fatehbensaali@gmail.com>
2026-09-21 20:12:51 +0100

    Test CI/CD: change version

--- src/myapp/app.py
+++ src/myapp/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return f"Hello 22 from myapp! Version: {os.environ.get('APP_VERSION', 'v1')}\n"
+    return f"i did change my version to :  Version: {os.environ.get('APP_VERSION', 'v1')}\n"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
