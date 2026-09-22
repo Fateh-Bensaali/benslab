c1f86bb83903b15bdb3a4434e18afa5e4f6e75e7
Fateh Bensaali <fatehbensaali@gmail.com>
2026-09-22 18:14:03 +0100

    new day

--- src/myapp/app.py
+++ src/myapp/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return f"i did change my version to :  Version: {os.environ.get('APP_VERSION', 'v1')}\n"
+    return f"Welcome back 22092026 :  Version: {os.environ.get('APP_VERSION', 'v1')}\n"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
